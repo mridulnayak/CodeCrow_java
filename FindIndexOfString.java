97b9bdfa36508cf88a6493a6fb6ccf09544b1ddd
Mridul Nayak <mridul122005@gmail.com>
2025-08-01 18:11:12 +0530

    Add files via upload

---
+++ FindIndexOfString.java
@@ -0,0 +1,8 @@
+class FindIndexOfString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
+       // Using indexOf() method to find the index of the first occurrence of needle in haystack
+// The strStr method returns the index of the first occurrence of needle in haystack
+// If needle is not found, it returns -1
