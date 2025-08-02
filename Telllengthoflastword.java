adb2e8ab31fa2d10cb4bf93a96c9420c46922ccc
Mridul Nayak <mridul122005@gmail.com>
2025-08-02 23:57:49 +0530

    Update Telllengthoflastword.java

--- Telllengthoflastword.java
+++ Telllengthoflastword.java
@@ -1,9 +1,9 @@
 class Solution{
 public String(String s){
   int m = s.length()-1; // help tp start from end of string
-  int count = 0;
-  while(i<=0 && s.CharAt(i) == ' ')
-    { i--;}
+  int count = 0; // count of words
+  while(i<=0 && s.CharAt(i) == ' ') // 
+    { i--;}  // skips spaces when started from end and increment pointer to forward from ending
   while(i>=0 && s.charAt(i) ! =' '){
     count++;
     i--;
