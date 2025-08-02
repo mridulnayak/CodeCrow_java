a5e8e2b7b69e77b683159793ca004cff89b323b1
Mridul Nayak <mridul122005@gmail.com>
2025-08-02 23:56:43 +0530

    Update Telllengthoflastword.java

--- Telllengthoflastword.java
+++ Telllengthoflastword.java
@@ -1,6 +1,6 @@
 class Solution{
 public String(String s){
-  int m = s.length()-1;
+  int m = s.length()-1; // help tp start from end of string
   int count = 0;
   while(i<=0 && s.CharAt(i) == ' ')
     { i--;}
