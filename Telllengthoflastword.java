6305328f13c4717812dd1bbca95adb6d6ecd53ba
Mridul Nayak <mridul122005@gmail.com>
2025-08-02 23:56:11 +0530

    Create Telllengthoflastword.java

---
+++ Telllengthoflastword.java
@@ -0,0 +1,20 @@
+class Solution{
+public String(String s){
+  int m = s.length()-1;
+  int count = 0;
+  while(i<=0 && s.CharAt(i) == ' ')
+    { i--;}
+  while(i>=0 && s.charAt(i) ! =' '){
+    count++;
+    i--;
+  }
+  return count;
+}
+
+
+ 
+    }
+        
+ 
+ 
+}
