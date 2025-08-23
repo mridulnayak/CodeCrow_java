47279403a6b344e5cd413cbad489b4c8f95f6190
Mridul Nayak <mridul122005@gmail.com>
2025-08-24 00:12:53 +0530

    Update LongestCommonPrefix.java

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,9 +1,10 @@
-class LongestCommonPrefix{
-  public String lcp(String [] strs){
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+     
     for(int i =0;i<strs[0].length();i++){
       char c = strs[0].charAt(i);
       for(int j=1;j<strs.length;j++){
-        if(i>=strs[j].length() || strs[j].chartAt(i) =!c){
+        if(i>=strs[j].length() || strs[j].charAt(i) !=c){
           return strs[0].substring(0,i);
         }
       }
@@ -13,4 +14,5 @@ class LongestCommonPrefix{
 }
       
 
-      
+          
+        
