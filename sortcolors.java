e83b6fb3fafa1fac53ab8cb1e364a656cdd78903
Mridul Nayak <mridul122005@gmail.com>
2025-08-25 01:12:43 +0530

    Update sortcolors.java

--- sortcolors.java
+++ sortcolors.java
@@ -1,21 +1,26 @@
 class Solution{
   public void sortColors(int [] nums){
-    int low = 0;
-    int mid = 0;
-    int high = nums.length-1;
-    while(mid<=high){
-      if(nums[mid] ==0){
+    int low = 0; // initialize mid and low 
+    int mid = 0; // low indicates left ( 0 place) mid indicates mid and its moving pointer as well as place of 1
+    int high = nums.length-1; // high is place of 2 and at righ most 
+// example [ 0 , 1 , 2 ]
+    
+    while(mid<=high){ // loop till mid is smaller/equal to high 
+      if(nums[mid] ==0){ // if element = 0, swap its place with low 
         int temp = nums[mid];
         nums[mid] = nums[low];
-        nums[low] = nums[high];
+        nums[low] = temp;
+        low++; // incremenet low and mid towards right to go through all elements 
+        mid++;
       }
-      else if(nums[mid] == 1){
+      else if(nums[mid] == 1){ // if first element = 1, its on right place just increment mid towards right
         mid++;
       }
       else{
-        int temp = nums[mid];
+        int temp = nums[mid]; // if element is 2 , swap its position with high place to right most
         nums[mid] = nums[high];
         nums[high] = temp;
+        high--
       }
     }
   }
